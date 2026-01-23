3cffb9391171b05a37ca844cb10b225c882ff0d9
Igarin <145533797+igarin14pm@users.noreply.github.com>
2026-01-23 13:52:36 +0900

    `AudioEngine`停止時にパラメーターを変更するとエラーが出るバグを修正

--- script.js
+++ script.js
@@ -378,13 +378,19 @@ function setUp() {
     modulatorValue.volumeUIValue = modulatorVolumeInputUI.value;
     
     visualFMSynth.modulator.volume = modulatorValue.volumeValue;
-    audioEngine.setParameterValue(modulatorValue.volumeParameterName, modulatorValue.volumeValue);
+    // audioEngine.setParameterValue(modulatorValue.volumeParameterName, modulatorValue.volumeValue);
+    if (audioEngine.isRunning) {
+      audioEngine.setParameterValue(modulatorValue.volumeParameterName, modulatorValue.volumeValue);
+    }
   });
   modulatorRatioInputUI.addEventListener(function() {
     modulatorValue.ratioUIValue = modulatorRatioInputUI.value;
     
     visualFMSynth.modulator.ratio = modulatorValue.ratioValue;
-    audioEngine.setParameterValue(modulatorValue.ratioParameterName, modulatorValue.ratioValue);
+    // audioEngine.setParameterValue(modulatorValue.ratioParameterName, modulatorValue.ratioValue);
+    if (audioEngine.isRunning) {
+      audioEngine.setParameterValue(modulatorValue.ratioParameterName, modulatorValue.ratioValue);
+    }
   })
   
   let synthFrameCallback = function() {
