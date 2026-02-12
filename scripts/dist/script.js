a1b43a3c4c6fc1baaa17543cf82d8b7b812608a3
Igarin <145533797+igarin14pm@users.noreply.github.com>
2026-02-12 03:50:36 +0000

    `AudioProcessor`を`FmSynthAudioProcessor`に名称変更

--- scripts/dist/script.js
+++ scripts/dist/script.js
@@ -100,8 +100,8 @@ class AudioEngine {
      */
     async start(modulatorValue, callback) {
         this.audioContext = new AudioContext();
-        await this.audioContext.audioWorklet.addModule('./scripts/dist/audio-processor.js');
-        this.audioWorkletNode = new AudioWorkletNode(this.audioContext, 'audio-processor');
+        await this.audioContext.audioWorklet.addModule('./scripts/dist/fm-synth-audio-processor.js');
+        this.audioWorkletNode = new AudioWorkletNode(this.audioContext, 'fm-synth-audio-processor');
         this.setParameterValue(modulatorValue.volumeParameterName, modulatorValue.volumeValue);
         this.setParameterValue(modulatorValue.ratioParameterName, modulatorValue.ratioValue);
         this.audioWorkletNode.connect(this.audioContext.destination);
