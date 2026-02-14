8dcaa63a9524d023e458cbee11e37e92abd6ea6e
Igarin <145533797+igarin14pm@users.noreply.github.com>
2026-02-14 02:48:35 +0000

    tsconfig.jsonのパスと内容を変更 コンパイル・動作を確認

--- assets/scripts/src/types/audio-worklet.d.ts
+++ assets/scripts/src/types/audio-worklet.d.ts
@@ -2,82 +2,83 @@
 // This software is released under the MIT License.
 // https://opensource.org
 
-/**
- * `AudioWorkletNode`上でカスタム`AudioParam`を作成するときに使用します
- */
-export interface AudioParamDescriptor {
+declare global {
 
   /**
-   * `AudioParam`の名前を表す文字列
+   * `AudioWorkletNode`上でカスタム`AudioParam`を作成するときに使用します
    */
-  name: string;
+  interface AudioParamDescriptor {
+
+    /**
+     * `AudioParam`の名前を表す文字列
+     */
+    name: string;
+
+    /**
+     * `AudioParam`の初期値
+     */
+    defaultValue?: number;
+
+    /**
+     * `AudioParam`の最小値
+     */
+    minValue?: number;
+
+    /**
+     * `AudioParam`の最大値
+     */
+    maxValue?: number;
+
+    /**
+     * `AudioParam`の自動化レートを表す`'a-rate'`・`'k-rate'`文字列のいずれか
+     */
+    automationRate?: 'a-rate' | 'k-rate';
+  }
 
   /**
-   * `AudioParam`の初期値
+   * `AudioWorkletNode`が用いる独自の音声処理コードを表します
    */
-  defaultValue?: number;
+  class AudioWorkletProcessor {
+
+    /**
+     * `AudioWorkletNode`との双方向通信に用いる`MessagePort`を返します
+     */
+    readonly port: MessagePort;
+
+    /**
+     * 音声処理アルゴリズムを表します
+     * @param inputs ノードに接続されている入力の配列 `inputs[n][m][i]`は`n`番目の入力の`m`番目のチャンネルの`i`番目のサンプルにアクセスします
+     * @param outputs 出力の配列 `process()`実行時に値を入力してください
+     * @param parameters `ParameterDescriptor`で定義した`AudioParam`オブジェクトについて、`name`がkey、値が`Float32Array`となります
+     * @returns 出力を継続するときに`true`、継続しないときに`false`
+     */
+    process(
+      inputs: Float32Array[][],
+      outputs: Float32Array[][],
+      parameters: Record<string, Float32Array>
+    ): boolean;
+  }
 
   /**
-   * `AudioParam`の最小値
+   * `AudioWorkletProcessor`から派生したクラスを指定の`name`で登録します
+   * @param name 登録する名前を表す文字列
+   * @param processorCtor 登録する`AudioWorkletProcessor`から派生したクラスのコンストラクタ
    */
-  minValue?: number;
+  function registerProcessor(
+    name: string,
+    processorCtor: new (options?: any) => AudioWorkletProcessor
+  ): void;
 
   /**
-   * `AudioParam`の最大値
+   * サンプルレートの値
    */
-  maxValue?: number;
+  var sampleRate: number;
 
   /**
-   * `AudioParam`の自動化レートを表す`'a-rate'`・`'k-rate'`文字列のいずれか
+   * 処理中の音声ブロックの時刻を表す数値
    */
-  automationRate?: 'a-rate' | 'k-rate';
+  var currentTime: number;
+
 }
 
-/**
- * `AudioWorkletNode`が用いる独自の音声処理コードを表します
- */
-export declare class AudioWorkletProcessor {
-
-  /**
-   * `AudioWorkletNode`との双方向通信に用いる`MessagePort`を返します
-   */
-  readonly port: MessagePort;
-
-  /**
-   * 音声処理アルゴリズムを表します
-   * @param inputs ノードに接続されている入力の配列 `inputs[n][m][i]`は`n`番目の入力の`m`番目のチャンネルの`i`番目のサンプルにアクセスします
-   * @param outputs 出力の配列 `process()`実行時に値を入力してください
-   * @param parameters `ParameterDescriptor`で定義した`AudioParam`オブジェクトについて、`name`がkey、値が`Float32Array`となります
-   * @returns 出力を継続するときに`true`、継続しないときに`false`
-   */
-  process(
-    inputs: Float32Array[][],
-    outputs: Float32Array[][],
-    parameters: Record<string, Float32Array>
-  ): boolean;
-}
-
-/**
- * `AudioWorkletProcessor`から派生したクラスを指定の`name`で登録します
- * @param name 登録する名前を表す文字列
- * @param processorCtor 登録する`AudioWorkletProcessor`から派生したクラスのコンストラクタ
- */
-export declare function registerProcessor(
-  name: string,
-  processorCtor: new (options?: any) => AudioWorkletProcessor
-): void;
-
-/**
- * サンプルレートの値
- */
-export declare var sampleRate: number;
-
-/**
- * 処理中の音声ブロックの時刻を表す数値
- */
-export declare var currentTime: number;
-
-/**
- * 複数の`AudioParam`のセット
- */
-export interface AudioParamMap extends ReadonlyMap<string, AudioParam> { }
+export {};
