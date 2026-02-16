be40b7663ba25ba5c155baa041070317a5668410
Igarin <145533797+igarin14pm@users.noreply.github.com>
2026-02-16 03:52:59 +0000

    assertion.tsのテストファイルを追加

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "roots": [
     "<rootDir>/assets/scripts/tests/unit"
   ],
+  "testEnvironment": "jsdom",
   "testMatch": [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
